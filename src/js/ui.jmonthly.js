14b6fbc8bbd7758f4cfc93b12a5aeefd5824d71e
Kyle Leneau <k.leneau@gmail.com>
2009-08-07 23:10:29 -0500

    Got the first parts of plumbing working, can _init the widget.

---
+++ src/js/ui.jmonthly.js
@@ -0,0 +1,45 @@
+/*!
+* Title:  jMonthly @VERSION
+* Dependencies:  jQuery 1.3.0 + ui.core.js
+* Author:  Kyle LeNeau
+* Email:  kyle.leneau@gmail.com
+* Project Hompage:  http://www.bytecyclist.com/projects/jmonthcalendar
+* Source:  http://github.com/KyleLeneau/jMonthly
+*
+*/
+(function($) {
+	
+$.widget("ui.jmonthly", {
+	
+	_init: function() {
+		
+		this.element.addClass("ui-widget ui-corner-all");
+	},
+	
+	changeMonth: function() {
+		alert(this.options.startDate);
+	},
+	
+	addEvent: function(collection) {
+		//add array or single
+	},
+	
+	removeEvents: function() {
+		//removes all events
+	},
+	
+	replaceEvents: function(collection) {
+		
+	}	
+	
+});
+
+$.extend($.ui.jmonthly, {
+	version: "0.0.1",
+	defaults: {
+		firstDayOfWeek: 0,
+		startDate: new Date()
+	}
+});
+	
+})(jQuery);
