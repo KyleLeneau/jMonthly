cd4d6d3aae4ee3d5a7d28766af30d3ed890fbabe
Kyle Leneau <k.leneau@gmail.com>
2009-08-08 13:19:44 -0500

    Added debug extension file to project to have detailed log statements.

---
+++ src/js/ui.jmonthly.debug.js
@@ -0,0 +1,17 @@
+/*!
+* Title:  jMonthly @VERSION
+* Dependencies:  jQuery 1.3.0 + ui.core.js
+* Author:  Kyle LeNeau
+* Email:  kyle.leneau@gmail.com
+* Project Hompage:  http://www.bytecyclist.com/projects/jmonthcalendar
+* Source:  http://github.com/KyleLeneau/jMonthly
+*
+*/
+(function($) {
+
+$.fn.log = function(msg) {
+	console.log("%s: %o", msg, this);
+	return this;
+};
+
+})(jQuery);
